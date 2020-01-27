5954e0a90abd20e499599ded8101daa0302e2ca2
Noemi1 <calmeida.no@gmail.com>
2020-01-27 11:31:50 -0300

    Organizando oprojet 2

--- src/app/shared/models/contas.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ContasModel = /** @class */ (function () {
-    function ContasModel() {
-    }
-    return ContasModel;
-}());
-exports.ContasModel = ContasModel;
--- src/app/shared/models/pessoas.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var PessoasModel = /** @class */ (function () {
-    function PessoasModel() {
-    }
-    return PessoasModel;
-}());
-exports.PessoasModel = PessoasModel;
